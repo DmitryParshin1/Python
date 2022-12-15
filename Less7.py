31a9704ec225ca218d9c8a78dfa8f77621cfd372
DmitryParshin <dima.rs@mail.ru>
2022-12-15 16:59:45 +0300

    добавил задачи

--- Less7.py
+++ Less7.py
@@ -5,3 +5,21 @@
 # - x=34; y=-30 -> 4
 # - x=2; y=4-> 1
 # - x=-34; y=-30 -> 3
+
+x = int(input(' Введите значение Х: '))
+y = int(input(' Введите значение Y: '))
+
+quarter = None
+
+if  x == 0 and y == 0:
+    print(' Введие X Y бoльше чем 0')
+if x > 0 and y > 0:
+    quarter = 1
+if x < 0 and y > 0:
+    quarter = 2
+if x < 0 and y < 0:
+    quarter = 3
+if x > 0 and y < 0:
+    quarter = 4
+
+print(f' Точка с координатами {x} и {y} находится в -> {quarter} четверти ')
